c8fd8a8a50107282bff77ab7289e5ccc19f7f55d
CholoCreator <v.ulices@gmail.com>
2025-11-13 11:23:48 +0100

    Segundos Cambios y React Instalación

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ngrok } from 'vite-plugin-ngrok'
-import process from 'node:process'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig({
+  plugins: [react()],
+});
 
-  return {
-    plugins: [
-      react(),
-      ngrok({
-        authtoken: env.NGROK_AUTH_TOKEN,
-      }),
-    ],
-    server: {
-      allowedHosts: ['.ngrok-free.app'],
-    },
-  }
-})
 
